b9e9f86ae12b1b3009f92896e311329dc3aa395b
shine <xiaoshaoqq@gmail.com>
2018-03-23 09:53:10 +0800

    update 2 add 3

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,11 @@ router.get('/no_connection_1', function(req, res, next) {
   res.render('No_Connection_1')
 });
 
+router.get('/2_404_error', function(req, res, next) {
+  res.render('2_404_Error')
+});
+router.get('/3_something_went_wrong', function(req, res, next) {
+  res.render('3_something_went_wrong')
+});
+
 module.exports = router;
